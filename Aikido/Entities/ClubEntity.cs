35c89893c20a7bbe91666edd54c090b5c3cec699
Артур <artur1741741@mail.ru>
2025-07-02 12:26:25 +0500

    Fix

--- Aikido/Entities/ClubEntity.cs
+++ Aikido/Entities/ClubEntity.cs
@@ -1,7 +1,16 @@
-﻿namespace Aikido.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aikido.Entities
 {
     public class ClubEntity : IDbEntity
     {
+        [Key]
         public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string Address { get; set; }
     }
 }
--- Aikido/Entities/EventEntity.cs
+++ Aikido/Entities/EventEntity.cs
@@ -1,7 +1,25 @@
-﻿namespace Aikido.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aikido.Entities
 {
     public class EventEntity : IDbEntity
     {
+        [Key]
         public long Id { get; set; }
+
+        public long UserId { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public string City { get; set; }
+
+
+        public string Title { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] File { get; set; }
     }
 }
