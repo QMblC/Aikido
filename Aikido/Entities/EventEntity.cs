6caccbcfb0b07a61893a1d8b91ce29cec6411bad
Артур <artur1741741@mail.ru>
2025-07-03 11:18:24 +0500

    creat club service

--- Aikido/Entities/EventEntity.cs
+++ Aikido/Entities/EventEntity.cs
@@ -6,5 +6,12 @@ namespace Aikido.Entities
     {
         [Key]
         public long Id { get; set; }
+        public long UserId { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string? City { get; set; }
+        public string? Title { get; set; }
+        public DateTime EventDate { get; set; }
+        public string? Description { get; set; }
+        public byte[] File { get; set; } = [];
     }
 }
---
+++ Aikido/Services/ClubService.cs
@@ -0,0 +1,74 @@
+using Aikido.Data;
+using Aikido.Dto;
+using Aikido.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aikido.Services
+{
+    public class ClubService
+    {
+        private readonly AppDbContext context;
+
+        public ClubService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        private async Task SaveDb()
+        {
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ошибка при обработке клуба: {ex.InnerException?.Message}", ex);
+            }
+        }
+
+        public async Task<ClubEntity> GetClubById(long id)
+        {
+            var clubEntity = await context.Clubs.FindAsync(id);
+            if (clubEntity == null)
+                throw new KeyNotFoundException($"Клуб с Id = {id} не найден.");
+
+            return clubEntity;
+        }
+
+        public async Task<long> CreateClub(ClubDto clubData)
+        {
+            var clubEntity = new ClubEntity();
+            clubEntity.UpdateFromJson(clubData);
+
+            context.Users.Add(clubEntity);
+
+            await SaveDb();
+
+            return clubEntity.Id;
+        }
+
+        public async Task DeleteClub(long id)
+        {
+            var clubEntity = await context.Clubs.FindAsync(id);
+            if (clubEntity == null)
+                throw new KeyNotFoundException($"Клуб с Id = {id} не найден.");
+
+            context.Remove(clubEntity);
+
+            await SaveDb();
+
+        }
+        public async Task UpdateClub(long id, ClubDto clubNewData)
+        {
+            var clubEntity = await context.Clubs.FindAsync(id);
+            if (clubEntity == null)
+                throw new KeyNotFoundException($"Клуб с Id = {id} не найден.");
+
+            clubEntity.UpdateFromJson(clubNewData);
+
+            await SaveDb();
+        }
+
+    }
+}
