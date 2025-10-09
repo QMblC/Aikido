05e4ab29198c3afaf9319322d05f0c2151a3e812
kurto <kurtovnevg@gmail.com>
2025-10-09 17:15:45 +0500

    fixed delete regulation endpoing

--- Aikido/Controllers/SeminarController.cs
+++ Aikido/Controllers/SeminarController.cs
@@ -143,7 +143,7 @@ namespace Aikido.Controllers
             }
         }
 
-        [HttpDelete("delete/{seminarId}/regulation")]
+        [HttpDelete("{seminarId}/regulation")]
         public async Task<IActionResult> DeleteSeminarRegulation(long seminarId)
         {
             try
